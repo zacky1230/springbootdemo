20af2381825e101b917a46078ab4fb4bb6787510
zacky1230 <zachary31@outlook.com>
2018-03-22 16:12:07 +0800

    el expression

---
+++ char02/src/main/java/com/zacky/char02/el/Main.java
@@ -0,0 +1,16 @@
+package com.zacky.char02.el;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(ElConfig.class);
+
+        ElConfig resourceService = context.getBean(ElConfig.class);
+
+        resourceService.outputResource();
+
+        context.close();
+    }
+}
